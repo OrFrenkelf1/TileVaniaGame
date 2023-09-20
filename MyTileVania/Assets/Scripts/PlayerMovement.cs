0771adb2f9f93fe385634fb931ff3b538e036562
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-09-20 22:42:01 +0300

    neeeeeeewwwwwwwwwww

--- MyTileVania/Assets/Scripts/PlayerMovement.cs
+++ MyTileVania/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 moveInput;
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask platformLayer;
 
-    void Start()
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    private void Update()
     {
-        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (!isFacingRight && horizontal > 0f)
+            Flip();
+        else if (isFacingRight && horizontal < 0f)
+            Flip();
     }
 
-    void Update()
+    public void Jump(InputAction.CallbackContext context)
     {
-        
+        if(context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }    
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 2f, platformLayer);
     }
-    
-    
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
 }
